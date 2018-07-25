dcdedce4718d4e418825e0a5dcd6a49e1d79b9ee
massa <massafranck@gmail.com>
2018-07-25 20:37:36 +0200

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+$ville = 'Compiègne';
+$numberInt = 10;
+$numberFloat = 5.5;
+$Boolean = true;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice4</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo $ville.'</br>'.$numberInt.'</br> '.$numberFloat.'</br>'.$Boolean;
+    ?>
+  </p>
+</body>
+</html>
